ffe7fd54c3714b3acb94a34927acc332a8d8f14b
keaidigou <37572127+keaidigou@users.noreply.github.com>
2020-11-07 17:30:08 +0800

    Update githist.sh

--- githist.sh
+++ githist.sh
@@ -1,14 +1,13 @@
-  506  mkdir ./gits
-  508  cd gits/
-  509  yum install git -y
-  512  git init 
-  515  git status 
-  516  git add .
-  517  git status 
-  518  git commit -m "ThirdGradeLinuxServer20201024"
-  519  git status 
-  520  git remote add origin https://github.com/keaidigou/LinuxServer.git
-  521  git remote -v
-  522  git status 
-  523  git push origin master 
-  524  history | grep git >> githist.sh
+mkdir ./gits
+cd gits/
+yum install git -y
+git init 
+git status 
+git add .
+git status 
+git commit -m "ThirdGradeLinuxServer20201024"
+git status 
+git remote add origin https://github.com/keaidigou/LinuxServer.git
+git remote -v
+git status 
+git push origin master 
